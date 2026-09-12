cc8a1debfc6207628b711b6832d24c68eaf6a458
Riswan <shamseernpm@gmail.com>
2026-09-12 13:48:14 +0530

    changed commit

--- one.dart
+++ one.dart
@@ -1,252 +1,276 @@
-
 void main() {
+ 
 
+  int li = 10;
+  print(li);
 
- int li=10; 
+  li = 20;
   print(li);
-  li=20;
-  print(li);
-  const n=20;
+
+  const n = 20;
   print(n);
-  var c=40;
-  c=50;
+
+  var c = 40;
+  c = 50;
   print(c);
-  dynamic f=80;
+
+  dynamic f = 80;
   print(f);
-  f="riswan";
+
+  f = "riswan";
   print(f);
-  final d=50;
+
+  final d = 50;
   print(d);
 
-  Symbol x= #f;
+  Symbol x = #f;
   print(x);
 
 
 
-  
-
- String name=" riswan ";
+  String name = " riswan ";
   print(name);
+
   print(name.length);
   print(name.isNotEmpty);
   print(name.contains("r"));
-  print(name.startsWith("r"));
-  print(name.endsWith("n"));
-  var std=name.trim();
+
+  
+  var std = name.trim();
+
+  print(std.startsWith("r"));
+  print(std.endsWith("n"));
+
   print(std);
-  print(std.replaceAll("riswan","sanaa"));
-  print(std.replaceAll("sanaa","riswan"));
-  print(std.substring(0,3));
+
+  print(std.replaceAll("riswan", "sanaa"));
+  print(std.replaceAll("sanaa", "riswan"));
+
+  print(std.substring(0, 3));
   print(std.indexOf("r"));
   print(std.toUpperCase());
 
 
 
+  Set<int> one = {1, 2, 3, 4};
+  Set<int> two = {4, 5, 6, 7};
 
-Set<int> one={1,2,3,4};
-Set<int> two={4,5,6,7};
+  var unionSet = one.union(two);
+  print(unionSet);
 
-var c=one.union(two);
-print(c);
-var d=one.difference(two);
-print(d);
-var e=two.difference(one);
-print(e);
-var f=one.intersection(two);
-print(f);
+  var differenceSet = one.difference(two);
+  print(differenceSet);
 
+  var differenceSet2 = two.difference(one);
+  print(differenceSet2);
 
+  var intersectionSet = one.intersection(two);
+  print(intersectionSet);
 
+  
 
-void greet(){
-List<int> z = [1, 2, 3, 4, 5, 6, 7];
+  void greet() {
+    List<int> z = [1, 2, 3, 4, 5, 6, 7];
 
-int count = 0;
+    int count = 0;
 
-for (int l = 0; l < z.length; l++) {
-  if (z[l] % 2 == 0) {
-    count++;
+    for (int l = 0; l < z.length; l++) {
+      if (z[l] % 2 == 0) {
+        count++;
+      }
+    }
+
+    print(count);
   }
-}
 
-print(count);
+  greet();
 
- }
+ 
 
+  void add() {
+    List<int> m = [10, 25, 45, 30, 50];
 
-greet();
+    int largest = 0;
+    int second = 0;
 
+    for (int i = 0; i < m.length; i++) {
+      if (m[i] > largest) {
+        second = largest;
+        largest = m[i];
+      } else if (m[i] > second) {
+        second = m[i];
+      }
+    }
 
-  void add(){
-List<int> m = [10, 25, 45, 30, 50];
-
-int largest = 0;
-int second = 0;
-
-for (int i = 0; i < m.length; i++) {
-  if (m[i] > largest) {
-    second = largest;
-    largest = m[i];
-  } else if (m[i] > second) {
-    second = m[i];
+    print(largest);
+    print(second);
   }
-}
 
-print(largest);
-print(second);
-  };
-
-add();
+  add();
 
 
 
- List<String> lii=["riswan","ashmil"];
- print(lii);
+  List<String> lii = ["riswan", "ashmil"];
 
- print(lii.isEmpty);
- 
- print(lii.first);
- 
- print(lii.last);
- 
- lii.add("jihan");
- print(lii);
- 
- lii.insert(0,"navya");
- print(lii);
- 
- lii.insertAll(1,["minha","fidha"]);
- print(lii);
- 
- lii.shuffle();
- print(lii);
- 
- lii.remove("fidha");
- print(lii);
- 
- lii.removeAt(4);
- print(lii);
+  print(lii);
 
- lii.addAll(["minha","fidha","nadha"]);
- 
- lii.forEach((value){
-  print(value);
- });
+  print(lii.isEmpty);
 
- lii.removeWhere((value)=> value.length==6);
- print(lii);
+  print(lii.first);
 
-lii.sort();
-print(lii);
+  print(lii.last);
 
-lii.clear();
-print(lii);
+  lii.add("jihan");
+  print(lii);
+
+  lii.insert(0, "navya");
+  print(lii);
+
+  lii.insertAll(1, ["minha", "fidha"]);
+  print(lii);
+
+  lii.shuffle();
+  print(lii);
+
+  lii.remove("fidha");
+  print(lii);
 
 
+  lii.remove("jihan");
+  print(lii);
 
-  List<int> see=[1,2,3,4,5];
+  lii.addAll(["minha", "fidha", "nadha"]);
 
-  for(int j=see.length-1; j>=0; j--){
+  lii.forEach((value) {
+    print(value);
+  });
+
+  lii.removeWhere((value) => value.length == 6);
+  print(lii);
+
+  lii.sort();
+  print(lii);
+
+
+
+
+
+  List<int> see = [1, 2, 3, 4, 5];
+
+  List<int> sece = [];
+
+  for (int j = see.length - 1; j >= 0; j--) {
     sece.add(see[j]);
   }
 
-print(sece);
+  print(sece);
 
-  for(int value in see){
-   if(value==1){
-    print("monday");
-   }else if(value==2){
-    print("tuesday");
-   }else if(value==3){
-    print("wednestday");
-   }else if(value==4){
-    print("thursday");
-   }else if(value==5){
-    print("friday");
-   }
+  
+
+  for (int value in see) {
+    if (value == 1) {
+      print("Monday");
+    } else if (value == 2) {
+      print("Tuesday");
+    } else if (value == 3) {
+      print("Wednesday");
+    } else if (value == 4) {
+      print("Thursday");
+    } else if (value == 5) {
+      print("Friday");
+    }
   }
 
+ 
 
+  void chea(List<String> fir, int sece) {
+    for (String value in fir) {
+      if (value.length == sece) {
+        print(value);
+      }
+    }
+  }
 
+  chea(["mango", "apple", "orange", "banana"], 5);
 
+ 
 
-void chea(List<String> fir,int sece){
-  for(String value in fir){
-    if(value.length==sece){
+  for (int k = 1; k <= 10; k += 2) {
+    print("$k odd   ${k + 1} even");
+  }
+
+  
+
+  List<String> names = [
+    "Arun",
+    "Riswan",
+    "Ashmil",
+    "Jihan",
+    "Anu"
+  ];
+
+  for (String value in names) {
+    if (value.startsWith("A")) {
       print(value);
     }
   }
-}
 
-chea(["mango","apple","orange","banana"],5);
+ 
 
+  List<String> namesss = [
+    "apple",
+    "banana",
+    "cat",
+    "orange",
+    "dog"
+  ];
 
-
-for(int k=1; k<=10; k+=2){
-  print('$k odd   ${k+1} even');
-}
-
-
-List<String> names = ["Arun", "Riswan", "Ashmil", "Jihan", "Anu"];
-
-for(String vaaluee in names){
-  if(vaaluee.startsWith("A")){
-    print(vaaluee);
+  for (String zz in namesss) {
+    if (zz.length > 5) {
+      print(zz);
+    }
   }
-}
+ 
 
 
+  List<String> nnaames = [
+    "Ali",
+    "Riswan",
+    "Ashmil",
+    "Jihan"
+  ];
 
-List<String> namesss = ["apple", "banana", "cat", "orange", "dog"];
+  String laarr = "";
 
-for(String zz in namesss){
-  if(zz.length>5){
-   print(zz);
+  for (String cc in nnaames) {
+    if (cc.length > laarr.length) {
+      laarr = cc;
+    }
   }
-}
 
+  print(laarr);
 
+ 
 
+  String p = "riswan";
 
-
-List<String> nnaames = ["Ali", "Riswan", "Ashmil", "Jihan"];
-
-String laarr="";
-
-for(String cc in nnaames){
-  if(cc.length>laarr.length){
-    laarr=cc;
+  for (int m = 0; m < p.length; m++) {
+    if (p[m] == "a" ||
+        p[m] == "e" ||
+        p[m] == "i" ||
+        p[m] == "o" ||
+        p[m] == "u") {
+      print(p[m]);
+    }
   }
-}
 
-print(laarr);
+ 
+  String ff = "riswan";
+  String gg = "";
 
+  for (int o = ff.length - 1; o >= 0; o--) {
+    gg = gg + ff[o];
+  }
 
-String p="riswan";
-
-for(int m=0; m<=p.length-1; m++){
-if(p[m]=="p"||
-   p[m]=="e"||
-   p[m]=="i"||
-   p[m]=="o"||
-   p[m]=="u"){
-    print(p[m]);
-   }
-}
-
-
-
-
-
-String ff="riswan";
-String gg="";
-for(int o=ff.length-1; o>=0; o--){
-  gg= gg+ff[o];
-}
-
-print(gg);
-
-
-
-
+  print(gg);
 }
