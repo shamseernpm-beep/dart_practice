aec4eb1c8139f6238d424ef1e9b6c004f79af88c
Riswan <shamseernpm@gmail.com>
2026-09-12 16:12:11 +0530

    advance leval practice

---
+++ advanced.dart
@@ -0,0 +1,45 @@
+void main(){
+    // NULL SAFETY
+
+    //?
+    //!
+    //??
+    //??=
+    //?.
+    //?..
+
+    //this all are mainly null safrty symbols
+    
+
+
+
+
+    String name="riswan";
+    print(name);
+
+    int? age=null;
+
+    print(age);
+
+
+    String? place=null;
+    print(place?? "its null value");
+
+    String? cours="flutter";
+    print(cours!.length);
+
+    late double hight;
+
+
+    hight=7.2;
+    print(hight);
+
+    String? a=null;
+
+    a??="its also null value";
+
+    print(a);
+
+    
+
+}
