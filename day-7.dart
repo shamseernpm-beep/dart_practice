8fb84dc55f2305b5cff94f4ed7b4a6f1b618a946
Riswan <shamseernpm@gmail.com>
2026-09-19 10:09:14 +0530

    day-7

--- day-7.dart
+++ day-7.dart
@@ -25,4 +25,27 @@ print(age?? 18);
 
 String? course="flutter developer";
 print(course!.length);
+
+int? balance=10000;
+print(balance?.isEven);
+
+
+List<String>? names=["riswan","ashmil"];
+
+names!
+
+  ..add("jihan")
+  ..add("navya")
+  ..add("minha");
+
+print(names);
+
+List<int>? num=[10,20,30];
+
+num
+ ?..add(40)
+ ..add(50);
+
+print(num);
+
 }
